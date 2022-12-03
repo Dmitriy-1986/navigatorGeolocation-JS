8135168ce880a5cc6f8d3dccd492f9aefe71a608
Dovgal Dima <52016913+Dmitriy-1986@users.noreply.github.com>
2022-12-03 15:55:53 +0200

    Update script.js

--- script.js
+++ script.js
@@ -12,6 +12,9 @@ if(navigator.geolocation) {
             document.body.innerHTML =   `Перейти на карту по вашим координатам: 
                                             <a href="https://www.google.com.ua/maps/place/${latitude}N+${longitude}E/">
                                                 ${latitude}, ${longitude}
+                                        </a><br>
+                                        <a href="viber://forward?text=https://www.google.com.ua/maps/place/${latitude}N+${longitude}E/">
+                                            Поделиться координатами в Вайбер
                                         </a>`;
         },
         function () {
